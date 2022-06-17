4902616d0fe12fb2c6d397fcc00393e4def1de01
cffricks <99102201+CFFricks@users.noreply.github.com>
2022-06-17 13:18:44 -0300

    exercicio 5 concluido

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/Exercicio 2/exercicio2.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/Exercicio 2/exercicio2.js
@@ -17,18 +17,3 @@ function calc(){
     };
 
 
-    // function ExecutarEx2(){
-    //     let n = document.getElementById("n").value;
-    //     if(Number(n)>0){
-    //         let resultado = document.getElementById("resultado");
-    //         n = n.split("");
-    //         let soma = 0;
-    //         for(i=0; i<n.length; i++){
-    //             soma += parseInt(n[i]);
-    //         };
-    //         resultado.innerHTML = "A soma dos algarismos é " + soma;
-    //     }
-    //     else{
-    //         resultado.innerHTML = "Apenas números positivos!"
-    //     };
-    // };
